e03be709e91052adb6f9741f2e1c005f7bd77108
Jovian Nanda <joviansismawan13@gmail.com>
2022-11-13 23:12:45 +0800

    Update 23:12

---
+++ animasi.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('terlihat');
+        }else{
+            entry.target.classList.remove('terlihat');
+        }
+    });
+});
+
+const elemenTersembunyi = document.querySelectorAll(".tersembunyi");
+elemenTersembunyi.forEach((element) => observer.observe(element));
