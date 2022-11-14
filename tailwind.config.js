3c0e1caa01085bcccb2b7357486fae3e8e900f75
Jovian Nanda <joviansismawan13@gmail.com>
2022-11-14 10:18:37 +0800

    Updated 10:18

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["*.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
